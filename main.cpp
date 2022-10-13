e9872d953c093bc295f04b523b51889f3fc935a9
jschreye <jschreye@c1r4s2.42lausanne.ch>
2022-10-13 17:19:21 +0200

    ca avance gentiment mais surment

--- main.cpp
+++ main.cpp
@@ -6,29 +6,37 @@
 
 int main()
 {
-    std::vector<int> x;
+    ft::vector<int> x;
     ft::vector<int> y;
     x.push_back(1);
     x.push_back(2);
     x.push_back(3);
-    x.push_back(4);
-    y.push_back(1);
-    y.push_back(2);
-    y.push_back(3);
-    y.push_back(4);
-    y.push_back(5);
-    std::cout << y.size() << std::endl;
+    y.push_back('a');
+    y.push_back('b');
+    y.push_back('z');
+    /*
     std::cout << "The first character is x '" << x.front() << "'.\n";
     std::cout << "The first character is x '" << x.back() << "'.\n";
     std::cout << "The first character is y '" << y.front() << "'.\n";
-    std::cout << "The first character is y '" << y.back() << "'.\n";/*
+    std::cout << "The first character is y '" << y.back() << "'.\n";
     x.pop_back();
     y.pop_back();
-    //std::cout << "The first character is x'" << x.front() << "'.\n";
-    //std::cout << "The first character is x'" << x.back() << "'.\n";
-    std::cout << "The first character is y'" << y.front() << "'.\n";
-    std::cout << "The first character is y'" << y.back() << "'.\n";
+    std::cout << "The first character is x '" << x.front() << "'.\n";
+    std::cout << "The first character is x '" << x.back() << "'.\n";
+    std::cout << "The first character is y '" << y.front() << "'.\n";
+    std::cout << "The first character is y '" << y.back() << "'.\n";
     y.push_back('c');
-    std::cout << "The first character is y'" << y.back() << "'.\n";*/
+    std::cout << "The first character is y '" << y.back() << "'.\n";
+    */
+    swap(x, y);
+    std::cout << "The first character is x '" << x.front() << "'.\n";
+    std::cout << "The first character is x '" << x.back() << "'.\n";
+    std::cout << "The first character is y '" << y.front() << "'.\n";
+    std::cout << "The first character is y '" << y.back() << "'.\n";
+    y.swap(x);
+    std::cout << "The first character is x '" << x.front() << "'.\n";
+    std::cout << "The first character is x '" << x.back() << "'.\n";
+    std::cout << "The first character is y '" << y.front() << "'.\n";
+    std::cout << "The first character is y '" << y.back() << "'.\n";
     return (0);
 }
--- vector.hpp
+++ vector.hpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 #include <memory>
-
+#include <vector>
 namespace ft
 {
     template < class T, class Alloc = std::allocator<T> >
@@ -26,10 +26,10 @@ namespace ft
 
         private:
 
-            allocator_type  _space;
-            pointer         _vector;
-            size_type       _capacity;
-            size_type       _size;
+            allocator_type  _space; //l'allocation memoir
+            pointer         _vector; //pointeur sur tableau
+            size_type       _capacity; //taille max de la memoir allouee
+            size_type       _size; //taille en cour
         
         private:
 
@@ -63,75 +63,135 @@ namespace ft
             }
 
             //destructor
-            ~vector()                                                                   {}//PAS FINI
+            ~vector() {}//PAS FINI
 
             //operator assignement
-            vector& operator=(const vector& rhs)                                        {
-                                                                                            if(this == &rhs)
-                                                                                                return *this;
-                                                                                        }//PAS FINI
+            vector& operator=(const vector& rhs)
+            {
+                if(this == &rhs)
+                    return *this;
+            }//PAS FINI
 
             //iterator
-            iterator begin()                                                            {return iterator(this->_vector);}
-            const_iterator begin() const                                                {return const_iterator(this->_vector);}
-            iterator end()                                                              {return iterator(this->_vector + this->_size);}
-            const_iterator end() const                                                  {return const_iterator(this->_vector + this->_size);}
-            reverse_iterator rbegin()                                                   {return reverse_iterator(iterator(this->end()));}
-            const_reverse_iterator rbegin() const                                       {return const_reverse_iterator(iterator(this->end));}
-            reverse_iterator rend()                                                     {return reverse_iterator(iterator(this->begin()));}
-            const_reverse_iterator rend() const                                         {return const_reverse_iterator(iterator(this->begin()));}
+            iterator begin()                        {return iterator(this->_vector);}
+            const_iterator begin() const            {return const_iterator(this->_vector);}
+            iterator end()                          {return iterator(this->_vector + this->_size);}
+            const_iterator end() const              {return const_iterator(this->_vector + this->_size);}
+            reverse_iterator rbegin()               {return reverse_iterator(iterator(this->end()));}
+            const_reverse_iterator rbegin() const   {return const_reverse_iterator(iterator(this->end));}
+            reverse_iterator rend()                 {return reverse_iterator(iterator(this->begin()));}
+            const_reverse_iterator rend() const     {return const_reverse_iterator(iterator(this->begin()));}
 
             //capacity
-            size_type size() const                                                      {return this->_size;}
-            size_type max_size() const                                                  {return _space.max_size();}
-            void resize (size_type n, value_type val = value_type());
-            size_type capacity() const                                                  {return this->_capacity;}
-            bool empty() const                                                          {return this->_size == 0;}
-            void reserve (size_type n);
+            size_type size() const                                      {return this->_size;}
+            size_type max_size() const                                  {return _space.max_size();}
+            void resize (size_type n, value_type val = value_type()); //A FAIRE
+            size_type capacity() const                                  {return this->_capacity;}
+            bool empty() const                                          {return this->_size == 0;}
+            void reserve (size_type n)
+            {
+                if (n > this->max_size())
+                    throw::std::length_error("The requested size is too high");
+                if (this->_capacity <= n)
+                {
+                    pointer tmp = _space.allocate(n);
+                    for (size_type i = 0; i < this->_size; i++) {
+                        _space.construct(tmp + i, *(this->_vector + i));
+                    }
+
+                    for (size_type i = 0; i < this->_capacity; i++) {
+                        _space.destroy(_vector + i);
+                    }
+                    _space.deallocate(this->_vector, this->_capacity);
+                    this->_vector = tmp;
+                    this->_capacity = n;
+                }
+            }
 
             //element access
-            reference operator[] (size_type n)                                          {return this->_vector[n];}
-            const_reference operator[] (size_type n) const                              {return this->_vector[n];}
-            reference at (size_type n);
-            const_reference at (size_type n) const;
-            reference front()                                                           {return this->_vector[0];}
-            const_reference front() const                                               {return this->_vector[0];}
-            reference back()                                                            {return this->_vector[this->_size - 1];}
-            const_reference back() const                                                {return this->_vector[this->_size - 1];}
-            value_type* data()                                                          {return this->_vector;}
-            const value_type* data() const                                              {return this->_vector;}
+            reference operator[] (size_type n)              {return this->_vector[n];}
+            const_reference operator[] (size_type n) const  {return this->_vector[n];}
+
+            reference at (size_type n)
+            {
+                if (n >= this->_size)
+                    throw std::out_of_range("Index used is out of range");
+                return (this->_vector[n]);
+            }
+
+            const_reference at (size_type n) const
+            {
+                if (n >= this->_size)
+                    throw std::out_of_range("Index used is out of range");
+                return (this->_vector[n]);
+            }
+
+            reference front()                               {return this->_vector[0];}
+            const_reference front() const                   {return this->_vector[0];}
+            reference back()                                {return this->_vector[this->_size - 1];}
+            const_reference back() const                    {return this->_vector[this->_size - 1];}
+            value_type* data()                              {return this->_vector;}
+            const value_type* data() const                  {return this->_vector;}
 
             //modifiers
             template <class InputIterator>
-            void assign (InputIterator first, InputIterator last);
+            void assign (InputIterator first, InputIterator last); //A FAIRE
             
-            void assign (size_type n, const value_type& val);
-            void push_back (const value_type& val)                                      {
-                                                                                            //if (this->_vector[this->_size - 1] == this->_capacity)
-                                                                                                //grow(capacity() + 1);
-                                                                                            this->_vector[this->size()] = val;
-                                                                                            this->_size++;
-                                                                                        }
-            void pop_back()                                                             {
-                                                                                            if (!(this->empty()))
-                                                                                            {
-                                                                                                this->_space.destroy(&this->back());
-                                                                                                this->_size--;
-                                                                                            }
-                                                                                        }
-            iterator insert (iterator position, const value_type& val);
-            void insert (iterator position, size_type n, const value_type& val);
+            void assign (size_type n, const value_type& val); //A FAIRE
+
+            void push_back (const value_type& val)                                      
+            {
+                if (this->_size == 0)
+                    this->reserve(1);
+                else if (this->_size + 1 > this->capacity())
+                    this->reserve(this->capacity() * 2);
+                _space.construct((this->_vector + this->size()), val);
+                this->_size++;
+            }
+
+            void pop_back()
+            {
+                if (!(this->empty()))
+                {  
+                    this->_space.destroy(&this->back());
+                    this->_size--;
+                }
+            }
+
+            iterator insert (iterator position, const value_type& val); //A FAIRE
+            void insert (iterator position, size_type n, const value_type& val); //A FAIRE
            
             template <class InputIterator>
-            void insert (iterator position, InputIterator first, InputIterator last);
+            void insert (iterator position, InputIterator first, InputIterator last); //A FAIRE
             
-            iterator erase (iterator position);
-            iterator erase (iterator first, iterator last);
-            void swap (vector<T,Alloc>& x);
-            void clear();
+            iterator erase (iterator position); //A FAIRE
+            iterator erase (iterator first, iterator last); //A FAIRE
+            void swap (vector<T,Alloc>& x)
+            {
+                allocator_type  _space_tmp = x._space;
+                pointer         _vector_tmp = x._vector;
+                size_type       _capacity_tmp = x._capacity;
+                size_type       _size_tmp = x._size;
 
+                x._space = this->_space;
+                x._vector = this->_vector;
+                x._capacity = this->_capacity;
+                x._size = this->_size;
+
+                this->_space = _space_tmp;
+                this->_vector = _vector_tmp;
+                this->_capacity = _capacity_tmp;
+                this->_size = _size_tmp;
+            }
+
+            void clear()
+            {
+                for (size_type i = 0; i != this->size(); i++)
+                    _space.destroy(_vector + i);
+                this->_size = 0;
+            }
             //allocator
-            allocator_type get_allocator() const                                        {return this->_space;}
+            allocator_type get_allocator() const    {return this->_space;}
 
     };
 
